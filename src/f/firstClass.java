2b7553852cd3b0b428a4d14835cce5335466f3aa
aliyaturuspayeva <turuspayeva@gmail.com>
2019-11-10 11:29:37 -0600

    test case

--- src/f/firstClass.java
+++ src/f/firstClass.java
@@ -4,6 +4,6 @@ public class firstClass {
 
     public static void main(String[] args) {
 
-        System.out.println("abc");
+        System.out.println("first");
     }
 }
---
+++ src/f/testCase5.java
@@ -0,0 +1,8 @@
+package f;
+
+public class testCase5 {
+    public static void main(String[] args) {
+
+        System.out.println("this is our test");
+    }
+}
